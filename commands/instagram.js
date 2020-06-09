159a67f9872fe2d57d72b2e0b7ec7fb9656a6198
Raahim Fareed <raahim1965@hotmail.com>
2020-06-09 23:48:28 +0500

    Instagram

--- commands/instagram.js
+++ commands/instagram.js
@@ -22,29 +22,41 @@ module.exports = {
         const igUsername = args[0].toLowerCase();
 
         const url = `http://www.instagram.com/${igUsername}/?__a=1`;
-        const res = await fetch(url).then(resUrl => resUrl.json());
+        const res = await fetch(url).then(resUrl => resUrl.json()).catch(err => {
+            console.log(`An error occurred: ${err}`);
+            return message.reply('Something wen\'t wrong! If this isn\'t fixed in the next hour, please appeal a complaint in my discord server!');
+        });
+            // .then(() => {
+
+            // })
+            // .catch(err => {
+            //     console.log(`An error occurred: ${err}`);
+            //     return message.reply('Something wen\'t wrong! If this isn\'t fixed in the next hour, please appeal a complaint in my discord server!');
+            // });
+
+            if (!res.graphql.user.username) return message.reply('I couldn\'t find an account :(');
+
+                const user = res.graphql.user;
+
+                const embedMsg = `**- Username:** ${user.username}\n
+                **- Full name:** ${user.full_name}\n
+                **- Biography:** ${user.biography.length == 0 ? 'none' : user.biography}\n
+                **- Posts:** ${user.edge_owner_to_timeline_media.count}\n
+                **- Followers:** ${user.edge_followed_by.count}\n
+                **- Following:** ${user.edge_follow.count}\n
+                **- Private Account:** ${user.is_private ? 'Yes 🔐' : 'No 🔓'}`;
+
+                const embed = new MessageEmbed()
+                    .setColor('RANDOM')
+                    .setAuthor(message.author.username, message.author.displayAvatarURL())
+                    .setTitle(user.full_name)
+                    .setURL(`http://www.instagram.com/${igUsername}/`)
+                    .setThumbnail(user.profile_pic_url_hd)
+                    .setDescription(embedMsg);
+
+                message.channel.send(embed);
 
         // console.log(res);
-
-        if (!res.graphql.user.username) return message.reply('I couldn\'t find an account :(');
-
-        const user = res.graphql.user;
-
-        const embed = new MessageEmbed()
-            .setColor('RANDOM')
-            .setAuthor(message.author.username, message.author.displayAvatarURL())
-            .setTitle(user.full_name)
-            .setURL(`http://www.instagram.com/${igUsername}/`)
-            .setThumbnail(user.profile_pic_url_hd)
-            .addField('Profile Information', `**- Username:** ${user.username} 
-            **- Full name:** ${user.full_name} 
-            **- Biography:** ${user.biography.length == 0 ? 'none' : user.biography} 
-            **- Posts:** ${user.edge_owner_to_timeline_media.count}
-            **- Followers:** ${user.edge_followed_by.count}
-            **- Following:** ${user.edge_follow.count}
-            **- Private Account:** ${user.is_private ? 'Yes 🔐' : 'No 🔓'}`);
-
-        message.channel.send(embed);
 
     },
 };
